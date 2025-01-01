96abb304075707cfe23aaac2c06342f55937d2a7
djedd1ne <mekki.djallaleddine@gmail.com>
2025-01-01 18:47:43 +0100

    add extract_product_info function

--- amazon_scraper/amazon_scraper.py
+++ amazon_scraper/amazon_scraper.py
@@ -3,10 +3,25 @@ import requests
 import csv
 import bs4
 
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36'
+REQUEST_HEADER = {
+  'User-Agent': USER_AGENT,
+  'Accept-Language': 'en-US, en;q=0.5',
+}
+
+def get_page_html(url):
+    res = requests.get(url=url, headers=REQUEST_HEADER)
+    return res.content
+
+def extract_product_info():
+    product_info = {}
+    print('Scraping URL: {url}')
+    html = get_page_html(url = url)
+    print(html)
+
 if __name__ == "__main__":
     with open('amazon_products_urls.csv', newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
         for row in reader:
             url = row[0]
-            print(url)
-
+            print(extract_product_info())
