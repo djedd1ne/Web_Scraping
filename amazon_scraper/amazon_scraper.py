c8845519f99b15617547f4c8e8dd2b770a569b02
djedd1ne <mekki.djallaleddine@gmail.com>
2025-01-01 15:00:33 +0100

    read urls from csv file

--- amazon_scraper/amazon_scraper.py
+++ amazon_scraper/amazon_scraper.py
@@ -4,5 +4,9 @@ import csv
 import bs4
 
 if __name__ == "__main__":
-    print("Hello")
+    with open('amazon_products_urls.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            url = row[0]
+            print(url)
 
